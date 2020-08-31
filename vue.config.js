088fde17e46acefe536794bbdaa86d7f3e2bf821
zc2513 <yyzc2513@163.com>
2020-08-31 10:20:32 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -117,5 +117,31 @@ module.exports = {
           config.optimization.runtimeChunk('single')
         }
       )
+  },
+  css: {
+    extract: false, // false表示开发环境,true表示生成环境
+    sourceMap: false,
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-px-to-viewport')({
+            unitToConvert: 'px',
+            viewportWidth: 1920,
+            viewportHeight: 1080,
+            unitPrecision: 3,
+            propList: [
+              '*'
+            ],
+            viewportUnit: 'vw',
+            fontViewportUnit: 'vw',
+            selectorBlackList: [],
+            minPixelValue: 1,
+            mediaQuery: false,
+            replace: true
+            // exclude: /(\/|\\)(node_modules)(\/|\\)/
+          })
+        ]
+      }
+    }
   }
 }
